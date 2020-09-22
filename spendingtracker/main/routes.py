c2a3684e962e6af1c8269522b21a060a643b03be
Adam <a.m.przyboroski@gmail.com>
2020-09-22 21:01:50 +0200

    feat(SpendingTracker): added user module fucnitons: register login/logout

--- spendingtracker/main/routes.py
+++ spendingtracker/main/routes.py
@@ -1,11 +1,9 @@
-from flask import Blueprint
-
-
+from flask import Blueprint, render_template
 
 main=Blueprint('main', __name__)
 
 
 @main.route('/')
 @main.route('/home')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template("layout.html")
