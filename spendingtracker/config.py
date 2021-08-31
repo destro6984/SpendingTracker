88c7b9d9b8ca16539bc82ebdec525a77ea0bbde8
Adam <a.m.przyboroski@gmail.com>
2021-08-31 13:37:57 +0200

    feat(spendingtracker): deploy preparation/ runtime added

--- spendingtracker/config.py
+++ spendingtracker/config.py
@@ -12,7 +12,7 @@ class Config:
 
 
 class ConfigProd(Config):
-    DEBUG = True
+    DEBUG = False
     S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
     S3_KEY = os.environ.get("S3_KEY")
     S3_SECRET_ACCESS_KEY = os.environ.get("S3_SECRET_ACCESS_KEY")
