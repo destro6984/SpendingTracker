d32679b7fd81508e3cf9b2f393db4b64fc66e806
Adam <a.m.przyboroski@gmail.com>
2021-10-05 17:04:33 +0200

    fix(spendingtracker):  connection to db v0.1

--- spendingtracker/main/routes.py
+++ spendingtracker/main/routes.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Blueprint, render_template, current_app
 
 from spendingtracker import db
@@ -8,6 +10,7 @@ main=Blueprint('main', __name__)
 @main.route('/')
 @main.route('/home')
 def home():
+    print(os.environ.get("DATABASE_URL"))
     map_site = ['%s' % rule for rule in current_app.url_map.iter_rules()]
     return render_template("layout.html",mapsite=[])
 
